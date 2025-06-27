ef4c2989952bef347a37ece6004a2e3859cc34a1
Jaime Muñoz <jaime.munozq@udea.edu.co>
2025-06-26 22:03:54 -0500

    fix: remove unnecessary newline in addContact step definition

--- features/step-definitions/addContactSteps.js
+++ features/step-definitions/addContactSteps.js
@@ -11,7 +11,7 @@ When(/^I add a new contact with (.*) and (.*) and (.*) and (.*) and (.*) and (.*
     async (firstName, lastName, birthDate, email, phone, street1, street2, city, state, postalCode, country) => {
         await addContactPage.addContact(firstName, lastName, birthDate, email, phone, street1, street2, city, state, postalCode, country);
     });
-
+ 
 Then(/^I should see a text saying (.*)$/, async (message) => {
     if (message === "Contact List") {
         await expect($("//h1[normalize-space()='Contact List']")).toBeExisting();
